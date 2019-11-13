28d563c737ac3ea64a50e90a12409ceffd093c2f
objektwerks <objektwerks@runbox.com>
2019-11-13 10:02:57 -0500

    edited store sql layout

--- src/main/scala/bh/Store.scala
+++ src/main/scala/bh/Store.scala
@@ -18,16 +18,18 @@ class Store(conf: Config) {
   logger.info(s"*** Store: Connected to Oracle store ( $url ).")
 
   def findDietNutritionByPatientEncounterId(patiendId: Long, encounterId: Long): Future[Option[DietNutrition]] = DB readOnly { implicit session =>
-    val result = sql"""select PERSON_ID as Patient,
-                 ENCNTR_ID as Encounter,
-                 pi_get_cv_display(ORDER_STATUS_CD) as Status,
-                 ORDER_MNEMONIC || ' ' || CLINICAL_DISPLAY_LINE as Diet
+    val result = sql"""
+                   select PERSON_ID as Patient,
+                   ENCNTR_ID as Encounter,
+                   pi_get_cv_display(ORDER_STATUS_CD) as Status,
+                   ORDER_MNEMONIC || ' ' || CLINICAL_DISPLAY_LINE as Diet
       from V500.ORDERS
       where ACTIVITY_TYPE_CD = 681598
       and CATALOG_TYPE_CD = 2511
       and PERSON_ID = $patiendId
       and ENCNTR_ID = $encounterId
-      and ACTIVE_IND = 1"""
+      and ACTIVE_IND = 1
+      """
       .map(rs => DietNutrition(rs.long("Patient"), rs.long("Encounter"), rs.string("Status"), rs.string("Diet")))
       .single
       .apply()
