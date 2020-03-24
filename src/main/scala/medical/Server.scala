c436333fcbc76b11a84638414e5d72a8ca95bc59
objektwerks <objektwerks@runbox.com>
2020-03-24 15:27:43 -0400

    added ssl support to server

--- src/main/scala/medical/Server.scala
+++ src/main/scala/medical/Server.scala
@@ -1,8 +1,12 @@
 package medical
 
+import java.io.InputStream
+import java.security.{KeyStore, SecureRandom}
+
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
+import akka.http.scaladsl.{ConnectionContext, Http, HttpsConnectionContext}
 import com.typesafe.config.ConfigFactory
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.Await
@@ -16,12 +20,26 @@ object Server {
     implicit val system = ActorSystem.create(conf.getString("server.name"), conf.getConfig("akka"))
     implicit val dispatcher = system.dispatcher
 
+
+    val password: Array[Char] = "bhsf".toCharArray
+    val keystore: KeyStore = KeyStore.getInstance("PKCS12")
+    val serverKey: InputStream = getClass.getClassLoader.getResourceAsStream("/server.key")
+    require(serverKey != null, "Keystore required!")
+    keystore.load(serverKey, password)
+    val keyManagerFactory: KeyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+    keyManagerFactory.init(keystore, password)
+    val trustManagerFactory: TrustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+    trustManagerFactory.init(keystore)
+    val sslContext: SSLContext = SSLContext.getInstance("TLS")
+    sslContext.init(keyManagerFactory.getKeyManagers, trustManagerFactory.getTrustManagers, new SecureRandom)
+    val https: HttpsConnectionContext = ConnectionContext.https(sslContext)
+
     val store = Store(conf)
     val router = Router(store)
     val host = Try(args(0)).getOrElse(conf.getString("server.host"))
     val port = Try(args(1).toInt).getOrElse(conf.getInt("server.port"))
     Http()
-      .bindAndHandle(router.api, host, port)
+      .bindAndHandle(router.api, host, port, connectionContext = https)
       .map { server =>
         logger.info(s"*** Server: ${server.localAddress.toString}")
       }
