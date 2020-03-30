d318662f1468674942d4993d859458f8c6ae8c0b
objektwerks <objektwerks@runbox.com>
2020-03-30 16:43:35 -0400

    added keystore-type

--- src/main/scala/medical/SSLContextFactory.scala
+++ src/main/scala/medical/SSLContextFactory.scala
@@ -6,10 +6,10 @@ import java.security.{KeyStore, SecureRandom}
 import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
 
 object SSLContextFactory {
-  def newInstance(passphrase: String, keystorePath: String): SSLContext = {
+  def newInstance(passphrase: String, keystorePath: String, keystoreType: String): SSLContext = {
     val inputstream = new FileInputStream(keystorePath)
     val password = passphrase.toCharArray
-    val keystore = KeyStore.getInstance("JKS")
+    val keystore = KeyStore.getInstance(keystoreType)
     keystore.load(inputstream, password)
     require(keystore != null, "Keystore is null. Load a valid keystore file.")
 
